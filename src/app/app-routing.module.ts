4f925252abf8e2d479a577ca31d6a109ade69c66
nsfe <nsfe0619@gmail.com>
2025-03-21 11:51:11 +0800

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Lesson1Component } from './pages/lesson1/lesson1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'lesson1', pathMatch: 'full' },
+  { path: 'lesson1', component: Lesson1Component }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
